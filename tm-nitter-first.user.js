c20cb88453259a5b1b3f344c69d36c7fc2556331
ZackDev <ZackDev@users.noreply.github.com>
2023-07-21 19:39:27 +0200

    update

--- tm-nitter-first.user.js
+++ tm-nitter-first.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nitter-first
 // @namespace    https://violentmonkey.github.io/
-// @version      0.6
+// @version      0.7
 // @description  replaces links to twitter.com with nitter.net
 // @match        *://*/*
 // @grant        none
@@ -12,7 +12,7 @@
 (function() {
     'use strict';
     // Your code here...
-    Array.from(document.getElementsByTagName('a')).filter(e => e.href.contains('twitter.com')).forEach((e) => {
+    Array.from(document.getElementsByTagName('a')).filter(e => e.href.includes('twitter.com')).forEach((e) => {
         e.href = e.href.replace('twitter.com', 'nitter.net');
         e.innerText = e.innerText.replace('twitter.com', 'nitter.net');
     });
