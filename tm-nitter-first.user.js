194853103381c19e3fe6f71cc0fbca00e5906280
ZackDev <ZackDev@users.noreply.github.com>
2023-08-21 19:58:57 +0200

    update

--- tm-nitter-first.user.js
+++ tm-nitter-first.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            nitter-first
 // @namespace       https://violentmonkey.github.io/
-// @version         0.30
+// @version         0.31
 // @description     replaces links to twitter.com with nitter.net
 // @match           https://*/*
 // @exclude-match   https://twitter.com/*
